fd6dbcd3fc4800b6902d548bbe72cb43735b18e7
Yadwinder Singh <93064634+Yadwinder-singh07@users.noreply.github.com>
2021-11-21 22:28:51 +0530

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -18,3 +18,28 @@
 #include <math.h>
 
 using namespace std;
+// creating macro for long long int as ll
+#define ll long long int
+#define pb push_back
+#define nn cout << "\n"
+#define tt cout << "\t"
+#define dbg(x) cout << #x << " = " << x << endl
+#define all(v) v.begin(), v.end()
+// typedefing for easier code writing and reading
+typedef vector<ll> vint;
+typedef vector<string> vector_string;
+
+// Function contains brief info about project
+void intro();
+// functions displays all the instructons regarding input and output
+void instructions();
+// functions displays collaborators names
+void ending();
+// functions erases all the input user has input
+// from user interface and displays only important info
+void invoking_one(ll num);
+// functions convert each expression user input after pressing 3
+// to long long int
+// for example input (abcd) = output(15);
+ll exp_to_minterms(ll sizes, string t);
+// class containting qm-solver functions
