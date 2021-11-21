b2694a6477cab4d590e6e603954c38ab8161f9ff
vinay-kumar07 <75077347+vinay-kumar07@users.noreply.github.com>
2021-11-21 22:27:54 +0530

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -1,1 +1,20 @@
+// CS203  Project
+/*
+ Vinay kumar        2020csb1141
+ Yadwinder Singh    2020csb1143
+*/
 
+// Header files
+#include <iostream>
+#include <stdio.h>
+#include <iterator>
+#include <stdlib.h>
+#include <string>
+#include <string.h>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <iomanip>
+#include <math.h>
+
+using namespace std;
